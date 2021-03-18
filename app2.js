7622a2475f393c0fe86b184480788b4a71e3ce44
LilitLalayan <76946852+LilitLalayan@users.noreply.github.com>
2021-03-18 16:26:21 +0400

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,20 @@
+const list = document.getElementById("list");
+const cityList = document.getElementsByClassName("cityList");
+
+async function getCityList() {
+  let myCities = [];
+  let res = await fetch(
+    "https://api.openbrewerydb.org/breweries/search?query=dog"
+  );
+  let cities = await res.json();
+
+  for (let i = 0; i < cities.length; i++) {
+    myCities.push(cities[i].city);
+  }
+  for (let i = 0; i < myCities.length; i++) {
+    let li = document.createElement("li");
+    li.append(myCities[i]);
+    list.append(li);
+  }
+}
+getCityList();
