4b06fbb06460aa33f98f86f7446bee53b926407a
LilitLalayan <76946852+LilitLalayan@users.noreply.github.com>
2021-03-18 16:36:58 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const search = document.getElementById("search");
+const city = document.getElementById("city");
+const result = document.getElementById("result");
+let a = document.getElementsByTagName("a");
+
+search.addEventListener("click", getBrewery);
+city.addEventListener("keydown", onKeyDown);
+
+async function getBrewery() {
+  if (result.hasChildNodes(a)) {
+    result.removeChild(a);
+  }
+  try {
+    let res = await fetch(
+      "https://api.openbrewerydb.org/breweries/search?query=dog"
+    );
+    let breweries = await res.json();
+    console.log(breweries);
+    let myBrew = breweries.filter((item) => item.city === city.value);
+    myBrew.forEach((el) => {
+      a = document.createElement("a");
+      a.href = el.website_url;
+      a.append(el.name);
+      result.append(a);
+    });
+  } catch (e) {
+    alert("Can not load");
+  }
+}
+
+async function onKeyDown(e) {
+  try {
+    if (e.key === "Enter") {
+      let res = await fetch(
+        "https://api.openbrewerydb.org/breweries/search?query=dog"
+      );
+      let breweries = await res.json();
+      console.log(breweries);
+      let myBrew = breweries.filter((item) => item.city === city.value);
+      myBrew.forEach((el) => {
+        a = document.createElement("a");
+        a.href = el.website_url;
+        a.append(el.name);
+        result.append(a);
+      });
+    }
+  } catch (e) {
+    alert(e.message);
+  }
+}
